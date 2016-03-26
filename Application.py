a86693f23f01639ef7de70a6b5f7654d4ebe6584
SrMouraSilva <mateus.moura@hotmail.com>
2016-03-26 17:24:14 +0000

    Fix import issue

--- Application.py
+++ Application.py
@@ -7,19 +7,18 @@ from controller.PluginsController import PluginsController
 class Application:
     controllers = {}
     
-    def __init__(self):
+    def __init__(self, dataPatch="data/"):
+        self.dataPatch = dataPatch
         controllers = [BanksController, CurrentController, DeviceController, PluginsController]
         
         for controller in controllers:
-            self.controllers[controller] = controller(self)
+            self.controllers[controller.__name__] = controller(self)
             
         for controller in self.controllers.values():
             controller.configure()
         
     def dao(self, dao):
-        DATA_PATH = "data/"
-        
-        return dao(DATA_PATH)
+        return dao(self.dataPatch)
     
     def controller(self, controller):
-        return self.controllers[controller]
+        return self.controllers[controller.__name__]
